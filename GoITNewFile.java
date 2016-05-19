d2de7a796c9571f2f169c2acf49892ea6e2a8933
Yevhenii Hadzhala-Odudenko <bukovinec4@gmail.com>
2016-05-19 17:27:35 +0700

    first commit

---
+++ GoITNewFile.java
@@ -0,0 +1,12 @@
+
+package goitnewfile;
+
+
+public class GoITNewFile {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+    
+}
